5e88a34b947cc7a19fd847b0ac44d8b54060afc3
maramkiran <maramkirankumar@gmail.com>
2015-06-30 13:58:07 +0530

    Testing

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,7 @@ angular.module('finalApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
-      'AngularJS'
+      'AngularJS',
+	'Karma'
     ];
   });
